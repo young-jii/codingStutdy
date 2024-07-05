d8cf8f8c4ee3e84039906cc48d030aaac064b12c
ji_young <150009335+young-jii@users.noreply.github.com>
2024-07-05 10:20:18 +0900

    [Bronze V] Title: 새싹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,13 @@
+line1 = "         ,r'\"7"
+line2 = "r`-_   ,'  ,/"
+line3 = " \\. \". L_r'"
+line4 = "   `~\\/"
+line5 = "      |"
+line6 = "      |"
+
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(line5)
+print(line6)
